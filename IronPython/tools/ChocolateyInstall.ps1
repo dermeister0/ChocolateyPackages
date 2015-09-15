380d0ff8c1e683d8ee2a3ccc23107d5f31b4ea83
Der_Meister <der@heavysoft.ru>
2015-09-15 22:37:06 +0700

    Update IronPython to version 2.7.5.

--- IronPython/tools/ChocolateyInstall.ps1
+++ IronPython/tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage 'ironpython' 'msi' '/quiet' 'http://ironpython.codeplex.com/downloads/get/723206' -checksum '54fe0ab22a2869ae53af983fe47d22d076ea1734' -checksumType 'sha1'
+﻿Install-ChocolateyPackage 'ironpython' 'msi' '/quiet' 'http://ironpython.codeplex.com/downloads/get/970325' -checksum '8F51858BE07D58F5D713EEB76A60268684D1471C' -checksumType 'sha1'
